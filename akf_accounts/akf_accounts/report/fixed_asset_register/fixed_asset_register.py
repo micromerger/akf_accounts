f4fe8e05d12f7127b6db310455c4082794b90642
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-01-07 12:42:46 +0000

    uncaught error fixed by nabeel.

--- akf_accounts/akf_accounts/report/fixed_asset_register/fixed_asset_register.py
+++ akf_accounts/akf_accounts/report/fixed_asset_register/fixed_asset_register.py
@@ -216,12 +216,12 @@ def prepare_chart_data(data, filters):
 
     for d in data:
         date = d.get(date_field)
-        belongs_to_month = formatdate(date, "MMM YYYY")
-
-        labels_values_map[belongs_to_month].asset_value += d.get("asset_value")
-        labels_values_map[belongs_to_month].depreciated_amount += d.get(
-            "depreciated_amount"
-        )
+        if(date):
+            belongs_to_month = formatdate(date, "MMM YYYY")
+            labels_values_map[belongs_to_month].asset_value += d.get("asset_value")
+            labels_values_map[belongs_to_month].depreciated_amount += d.get(
+                "depreciated_amount"
+            )
 
     return {
         "data": {
