b5c38d5395c095454c04d4cfdb10b1986a0ffb4e
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-03-19 08:54:06 +0000

    Donor Wise Fund Movement Report updated by Mubashir

--- akf_accounts/akf_accounts/report/donor_wise_fund_movement_report/donor_wise_fund_movement_report.js
+++ akf_accounts/akf_accounts/report/donor_wise_fund_movement_report/donor_wise_fund_movement_report.js
@@ -1,8 +1,90 @@
-// Copyright (c) 2024, Nabeel Saleem and contributors
-// For license information, please see license.txt
+// Mubashir Bashir 17-03-2025
 
 frappe.query_reports["Donor Wise Fund Movement Report"] = {
 	"filters": [
-
-	]
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname": "donor",
+			"label": __("Donor"),
+			"fieldtype": "Link",
+			"options": "Donor"
+		},		
+		{
+			"fieldname":"cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Cost Center', txt, {
+					company: frappe.query_report.get_filter_value("company")
+				});
+			}
+		},
+		{
+			"fieldname":"service_area",
+			"label": __("Service Area"),
+			"fieldtype": "Link",
+			"options": "Service Area"
+		},
+		{
+			"fieldname":"subservice_area",
+			"label": __("Subservice Area"),
+			"fieldtype": "Link",
+			"options": "Subservice Area"
+		},
+		{
+			"fieldname":"product",
+			"label": __("Product"),
+			"fieldtype": "Link",
+			"options": "Product"
+		},
+		{
+			"fieldname":"project",
+			"label": __("Project"),
+			"fieldtype": "Link",
+			"options": "Project"
+		},
+		{
+			"fieldname":"fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"reqd": 1,
+			"default": ""
+		},		
+	],
+	"onload": function(report) {
+		setTimeout(() => {
+			get_current_fiscal_year();
+		}, 500);
+	}
 };
+
+function get_current_fiscal_year() {
+	let today = frappe.datetime.get_today();
+
+	frappe.db.get_list("Fiscal Year", {
+		filters: [
+			["year_start_date", "<=", today],
+			["year_end_date", ">=", today]
+		],
+		fields: ["name"],
+		limit: 1
+	}).then((res) => {
+		if (res.length > 0) {
+			let fiscal_year = res[0].name;
+			console.log("Fiscal Year is", fiscal_year);
+
+			// Ensure the filter is available before setting it
+			setTimeout(() => {
+				frappe.query_report.set_filter_value("fiscal_year", fiscal_year);
+			}, 500);
+		}
+	});
+}
--- akf_accounts/akf_accounts/report/donor_wise_fund_movement_report/donor_wise_fund_movement_report.py
+++ akf_accounts/akf_accounts/report/donor_wise_fund_movement_report/donor_wise_fund_movement_report.py
@@ -1,66 +1,208 @@
-# Copyright (c) 2024, Nabeel Saleem and contributors
-# For license information, please see license.txt
+# Mubashir Bashir 17-03-2025
 
 import frappe
 from frappe import _
 
-
 @frappe.whitelist(allow_guest=True)
 def execute(filters=None):
-    columns, data = [], []
     columns = get_columns()
     data = get_data(filters)
     return columns, data
 
-
 def get_columns():
-    columns = [
-        _("Donor") + ":Data:140",
-        # _("OB") + ":Data:140",
-        # _("Debit") + ":Data:140",
-        # _("Credit") + ":Data:140",
-        # _("Balance") + ":Data:140",
+    return [
+        _("Company") + ":Link/Company:140",
+        _("Donor") + ":Link/Donor:140",
+        _("Cost Center") + ":Link/Cost Center:140",
+        _("Service Area") + ":Link/Service Area:140",
+        _("Subservice Area") + ":Link/Subservice Area:140",
+        _("Product") + ":Link/Product:140",
+        _("Project") + ":Link/Project:140",
+        _("Opening Balance") + ":Float:140",
+        _("Opening Receivable") + ":Float:140",
+        _("Inter-Fund Transfer From (Sent)") + ":Float:140",
+        _("Inter-Fund Transfer To (Rec.)") + ":Float:140",
+        _("Inter-Branch Transfer From(Exc. HO)") + ":Data:140",
+        _("Inter-Branch Transfer To(Exc. HO)") + ":Data:140",
+        _("Inter-Branch Transfer From(HO)") + ":Data:140",
+        _("Inter-Branch Transfer To(HO)") + ":Data:140",
+        _("Payment(Others)") + ":Data:140",
+        _("Restricted Income") + ":Float:140",
+        _("Admin Income") + ":Float:140",
+        _("Endowment") + ":Float:140",
+        _("Fund Raising") + ":Float:140",
+        _("Closing Balance") + ":Float:140",
+        _("Closing Receivable") + ":Float:140",
     ]
-    return columns
-
 
 def get_data(filters):
-    result = get_query_result(filters)
-    return result
+    fiscal_year_dates = get_fiscal_year_dates(filters)    
+    year_start_date, year_end_date = fiscal_year_dates
+    company = filters.get("company")
+    donor = filters.get("donor")
+    cost_centers = filters.get("cost_center")
+    service_area = filters.get("service_area")
+    subservice_area = filters.get("subservice_area")
+    product = filters.get("product")
+    project = filters.get("project")
+    
+    balance_data = {}  
 
+    # Dynamic WHERE clause for filters
+    where_conditions = ["d.company = %(company)s", "d.posting_date BETWEEN %(start_date)s AND %(end_date)s"]
+    query_params = {"company": company, "start_date": year_start_date, "end_date": year_end_date}
 
-def get_conditions(filters):
-    conditions = ""
+    if cost_centers:
+        where_conditions.append("d.donation_cost_center IN %(cost_center)s")
+        query_params["cost_center"] = tuple(cost_centers) if isinstance(cost_centers, list) else (cost_centers,)
+    if service_area:
+        where_conditions.append("pd.service_area = %(service_area)s")
+        query_params["service_area"] = service_area
+    if subservice_area:
+        where_conditions.append("pd.subservice_area = %(subservice_area)s")
+        query_params["subservice_area"] = subservice_area
+    if product:
+        where_conditions.append("pd.product = %(product)s")
+        query_params["product"] = product
+    if project:
+        where_conditions.append("pd.project_id = %(project)s")
+        query_params["project"] = project
+    if donor:
+        where_conditions.append("db.donor = %(donor)s")
+        query_params["donor"] = donor
 
-    # if filters.get("company"):
-    #     conditions += " AND company = %(company)s"
-    # if filters.get("applicant"):
-    #     conditions += " AND applicant = %(applicant)s"
-    # if filters.get("branch"):
-    #     conditions += " AND branch = %(branch)s"
-    # if filters.get("loan_type"):
-    #     conditions += " AND loan_type = %(loan_category)s"
-    # if filters.get("repayment_start_date"):
-    #     conditions += " AND repayment_start_date = %(repayment_start_date)s"
+    where_clause = " AND ".join(where_conditions)
 
-    return conditions
-
-
-def get_query_result(filters):
-    conditions = get_conditions(filters)
-    result = frappe.db.sql(
-        """
-        SELECT 
-			name            
-        FROM 
-            `tabGL Entry`
-        WHERE
-            docstatus != 2
-        {0}
-    """.format(
-            conditions if conditions else ""
-        ),
-        filters,
-        as_dict=0,
+    income_data = frappe.db.sql(
+        f"""
+        SELECT db.income_type, SUM(db.amount) AS amount, d.donation_cost_center, pd.service_area, pd.subservice_area, pd.product, pd.project_id, db.donor
+        FROM `tabDonation` AS d
+        INNER JOIN `tabPayment Detail` AS pd ON pd.parent = d.name
+        INNER JOIN `tabDeduction Breakeven` AS db ON db.parent = d.name AND db.random_id = pd.random_id
+        WHERE {where_clause} AND db.donor != ''
+        GROUP BY db.donor, db.income_type, d.donation_cost_center, pd.service_area, pd.subservice_area, pd.product, pd.project_id
+        """,
+        query_params,
+        as_dict=True
     )
-    return result
+
+    for entry in income_data:
+        key = (company, entry.get("donor"), entry.get("donation_cost_center"), entry.get("service_area"), entry.get("subservice_area"), entry.get("product"), entry.get("project_id"))
+        balance_data.setdefault(key, {
+            'opening_balance': 0, 'receivable_balance': 0,
+            'closing_balance': 0, 'closing_receivable': 0,
+            'inter_fund_sent': 0, 'inter_fund_received': 0,
+            'inter_branch_sent_exc_ho': 0, 'inter_branch_received_exc_ho': 0,
+            'inter_branch_sent_ho': 0, 'inter_branch_received_ho': 0,
+            'payment_others': 0,
+            'restricted_income': 0, 'admin_income': 0,
+            'endowment': 0, 'fund_raising': 0
+        })
+        balance_data[key][entry["income_type"].lower().replace(" ", "_")] = entry["amount"]
+
+    # Dynamic WHERE clause for gl_entries query
+    gl_where_conditions = ["company = %(company)s", "docstatus = 1", "posting_date <= %(end_date)s"]
+    gl_query_params = {"company": company, "end_date": year_end_date}
+
+    if cost_centers:
+        gl_where_conditions.append("cost_center IN %(cost_center)s")
+        gl_query_params["cost_center"] = tuple(cost_centers) if isinstance(cost_centers, list) else (cost_centers,)
+    if service_area:
+        gl_where_conditions.append("service_area = %(service_area)s")
+        gl_query_params["service_area"] = service_area
+    if subservice_area:
+        gl_where_conditions.append("subservice_area = %(subservice_area)s")
+        gl_query_params["subservice_area"] = subservice_area
+    if product:
+        gl_where_conditions.append("product = %(product)s")
+        gl_query_params["product"] = product
+    if project:
+        gl_where_conditions.append("project = %(project)s")
+        gl_query_params["project"] = project
+    if donor:
+        gl_where_conditions.append("donor = %(donor)s")
+        gl_query_params["donor"] = donor
+
+    gl_where_clause = " AND ".join(gl_where_conditions)
+
+    gl_entries = frappe.db.sql(
+        f"""
+        SELECT donor, account, debit, credit, cost_center, service_area, subservice_area, product, project, posting_date, voucher_type, voucher_no
+        FROM `tabGL Entry`
+        WHERE {gl_where_clause}
+        AND project != '' AND donor != ''
+        GROUP BY donor, cost_center, service_area, subservice_area, product, project
+        """,
+        gl_query_params,
+        as_dict=True
+    )
+
+    accounts = {acc.name: acc.account_type for acc in frappe.get_all("Account", fields=["name", "account_type"])}
+
+    for entry in gl_entries:
+        key = (company, entry.get("donor"), entry.get("cost_center"), entry.get("service_area"), entry.get("subservice_area"), entry.get("product"), entry.get("project"))
+        balance_data.setdefault(key, {
+            'opening_balance': 0, 'receivable_balance': 0,
+            'closing_balance': 0, 'closing_receivable': 0,
+            'inter_fund_sent': 0, 'inter_fund_received': 0,
+            'inter_branch_sent_exc_ho': 0, 'inter_branch_received_exc_ho': 0,
+            'inter_branch_sent_ho': 0, 'inter_branch_received_ho': 0,
+            'payment_others': 0,
+            'restricted_income': 0, 'admin_income': 0,
+            'endowment': 0, 'fund_raising': 0
+        })
+
+        balance = entry['credit'] - entry['debit']
+        if entry['posting_date'] < year_start_date:
+            balance_data[key]['opening_balance'] += balance
+        elif entry['posting_date'] <= year_end_date:
+            balance_data[key]['closing_balance'] += balance
+        
+        if entry['posting_date'] >= year_start_date and entry['posting_date'] <= year_end_date:
+            if entry['voucher_type'] == 'Payment Entry':
+                balance_data[key]['payment_others'] += entry['debit']
+            elif entry['voucher_type'] == 'Funds Transfer':
+                funds_transfer = frappe.get_doc('Funds Transfer', entry['voucher_no'], ['transaction_type', 'from_cost_center', 'to_cost_center'])
+                if funds_transfer.transaction_type == 'Inter Fund':
+                    balance_data[key]['inter_fund_sent'] += entry['debit']
+                    balance_data[key]['inter_fund_received'] += entry['credit']
+                elif funds_transfer.transaction_type == 'Inter Branch' and funds_transfer.from_cost_center != 'Main - AKFP':
+                    balance_data[key]['inter_branch_sent_exc_ho'] += entry['debit']
+                elif funds_transfer.transaction_type == 'Inter Branch' and funds_transfer.to_cost_center != 'Main - AKFP':
+                    balance_data[key]['inter_branch_received_exc_ho'] += entry['credit']
+                elif funds_transfer.transaction_type == 'Inter Branch' and funds_transfer.from_cost_center == 'Main - AKFP':
+                    balance_data[key]['inter_branch_sent_ho'] += entry['debit']
+                elif funds_transfer.transaction_type == 'Inter Branch' and funds_transfer.to_cost_center == 'Main - AKFP':
+                    balance_data[key]['inter_branch_received_ho'] += entry['credit']
+
+        if accounts.get(entry['account']) == 'Receivable':
+            if entry['posting_date'] < year_start_date:
+                balance_data[key]['receivable_balance'] += balance
+            elif entry['posting_date'] <= year_end_date:
+                balance_data[key]['closing_receivable'] += balance
+
+
+    # Converting dictionary data to report format
+    report_data = [
+        [
+            company, donor, cost_center, service_area, subservice_area, product, project,
+            balances['opening_balance'], balances['receivable_balance'],
+            balances['inter_fund_sent'], balances['inter_fund_received'],
+            balances['inter_branch_sent_exc_ho'], balances['inter_branch_received_exc_ho'],
+            balances['inter_branch_sent_ho'], balances['inter_branch_received_ho'],
+            balances['payment_others'],
+            balances['restricted_income'], balances['admin_income'],
+            balances['endowment'], balances['fund_raising'],
+            balances['closing_balance'], balances['closing_receivable']
+        ]
+        for (company, donor, cost_center, service_area, subservice_area, product, project), balances in balance_data.items()
+    ]
+
+    return report_data
+
+
+def get_fiscal_year_dates(filters):
+    if not filters.get("fiscal_year"):
+        return None
+    fiscal_year = frappe.get_doc("Fiscal Year", filters.get("fiscal_year"))
+    return fiscal_year.year_start_date, fiscal_year.year_end_date
