f159b640c1aac53269bc8d7bc29843330bdc7b01
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-04-10 11:04:57 +0000

    Fund Class Detail Report made by Mubashir Bashir

--- akf_accounts/akf_accounts/report/fund_class_detail_report/fund_class_detail_report.js
+++ akf_accounts/akf_accounts/report/fund_class_detail_report/fund_class_detail_report.js
@@ -1,8 +1,56 @@
-// Copyright (c) 2024, Nabeel Saleem and contributors
-// For license information, please see license.txt
+//  Mubashir Bashir 09-04-2025
 
 frappe.query_reports["Fund Class Detail Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"cost_center",
+			"label": __("Cost Center"),
+			"fieldtype": "MultiSelectList",
+			get_data: function(txt) {
+				return frappe.db.get_link_options('Cost Center', txt, {
+					company: frappe.query_report.get_filter_value("company")
+				});
+			}
+		},
+		{
+			"fieldname":"service_area",
+			"label": __("Service Area"),
+			"fieldtype": "Link",
+			"options": "Service Area"
+		},
+		{
+			"fieldname":"subservice_area",
+			"label": __("Subservice Area"),
+			"fieldtype": "Link",
+			"options": "Subservice Area"
+		},
+		{
+			"fieldname":"account",
+			"label": __("Account"),
+			"fieldtype": "Link",
+			"options": "Account"
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},	
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},	
 	]
 };
--- akf_accounts/akf_accounts/report/fund_class_detail_report/fund_class_detail_report.py
+++ akf_accounts/akf_accounts/report/fund_class_detail_report/fund_class_detail_report.py
@@ -1,69 +1,59 @@
-# Copyright (c) 2024, Nabeel Saleem and contributors
-# For license information, please see license.txt
+# Mubashir Bashir, 09-04-2025
 
 import frappe
 from frappe import _
 
-
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def execute(filters=None):
-    columns, data = [], []
     columns = get_columns()
     data = get_data(filters)
+
     return columns, data
 
-
 def get_columns():
-    columns = [
-        _("Code") + ":Data:140",
-        # _("Description") + ":Data:140",
-        # _("Type") + ":Data:140",
+    return [
+        _("Company") + ":Link/Company:140",
+        _("Cost Center") + ":Link/Cost Center:140",
+        _("Service Area") + ":Link/Service Area:180",
+        _("Subservice Area") + ":Link/Subservice Area:180",
+        _("Project") + ":Link/Project:180",
+        _("Project Name") + ":Data:180",
+        _("Project Type") + ":Data:180",
         # _("Inter-Fund Transfer Account") + ":Data:140",
-        # _("Amortization Account") + ":Data:140",
-        # _("Admin Income Account") + ":Data:140",
-        # _("Fund Equity Account") + ":Data:140",
-        # _("Status") + ":Data:140",
+        _("Amortization Account") + ":Data:140",
+        _("Admin Income Account") + ":Data:140",
+        _("Fund Equity Account") + ":Link/Account:140",
+        _("Account") + ":Link/Account:140",
+        _("Status") + ":Data:140",
     ]
-    return columns
-
 
 def get_data(filters):
-    result = get_query_result(filters)
-    return result
+    result = frappe.db.sql("""
+    SELECT gle.company, gle.cost_center, gle.service_area, gle.subservice_area, gle.project,
+           p.project_name, p.project_type,
+           CASE WHEN acc.root_type = 'Equity' THEN gle.account ELSE '' END AS amortization_account,
+           CASE WHEN acc.root_type = 'Income' THEN gle.account ELSE '' END AS adming_income_account,
+           CASE WHEN acc.root_type = 'Equity' THEN gle.account ELSE '' END AS fund_equity_account, 
+           p.status
+    FROM `tabGL Entry` as gle
+    LEFT JOIN `tabProject` as p ON p.name = gle.project
+    LEFT JOIN `tabAccount` as acc ON acc.name = gle.account
+    WHERE gle.docstatus = 1 AND gle.project IS NOT NULL AND gle.account IS NOT NULL {conditions}
+    GROUP BY gle.company, gle.cost_center, gle.service_area, gle.subservice_area, gle.project, gle.account
+    """.format(conditions = get_conditions(filters)), filters, as_dict=True)
 
+    return result
 
 def get_conditions(filters):
-    conditions = ""
+    conditions = ''
 
-    # if filters.get("company"):
-    #     conditions += " AND company = %(company)s"
-    # if filters.get("applicant"):
-    #     conditions += " AND applicant = %(applicant)s"
-    # if filters.get("branch"):
-    #     conditions += " AND branch = %(branch)s"
-    # if filters.get("loan_type"):
-    #     conditions += " AND loan_type = %(loan_category)s"
-    # if filters.get("repayment_start_date"):
-    #     conditions += " AND repayment_start_date = %(repayment_start_date)s"
+    if filters.get('company'): conditions += " AND gle.company = %(company)s"
+    if filters.get('cost_center'): conditions += " AND gle.cost_center = %(cost_center)s"
+    if filters.get('service_area'): conditions += " AND gle.service_area = %(service_area)s"
+    if filters.get('subservice_area'): conditions += " AND gle.subservice_area = %(subservice_area)s"
+    if filters.get('project'): conditions += " AND gle.project = %(project)s"
+    if filters.get('account'): conditions += " AND gle.account = %(account)s"
+    if filters.get('from_date') and filters.get('to_date'):
+        conditions += " AND gle.posting_date >= %(from_date)s AND gle.posting_date <= %(to_date)s"
 
     return conditions
-
-
-def get_query_result(filters):
-    conditions = get_conditions(filters)
-    result = frappe.db.sql(
-        """
-        SELECT 
-			name
-        FROM 
-            `tabGL Entry`
-        WHERE
-            docstatus != 2
-        {0}
-    """.format(
-            conditions if conditions else ""
-        ),
-        filters,
-        as_dict=0,
-    )
-    return result
