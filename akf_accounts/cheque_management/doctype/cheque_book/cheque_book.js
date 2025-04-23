0eabd5b374fbf759b8947d19a4404ed31d34f940
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-04-23 14:26:16 +0000

    cheque book.

--- akf_accounts/cheque_management/doctype/cheque_book/cheque_book.js
+++ akf_accounts/cheque_management/doctype/cheque_book/cheque_book.js
@@ -11,7 +11,7 @@ frappe.ui.form.on("Cheque Book", {
       __("New Cheque Book"),
       function () {
         if(frm.doc.branch==undefined || frm.doc.branch==""){
-            frappe.msgprint("Branch is required in `Bank Account` to proceed.")
+            frappe.msgprint("Branch is required in `` to proceed.", title="Bank Account")
         }else{
           frappe.call({
             method:
