4d887a8a19c775102b2f9697b1f8d6303e6cb29a
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-04-23 14:25:12 +0000

    cheque book.

--- akf_accounts/cheque_management/doctype/cheque_book/cheque_book.js
+++ akf_accounts/cheque_management/doctype/cheque_book/cheque_book.js
@@ -10,30 +10,34 @@ frappe.ui.form.on("Cheque Book", {
     frm.add_custom_button(
       __("New Cheque Book"),
       function () {
-        frappe.call({
-          method:
-            "akf_accounts.cheque_management.doctype.cheque_book.cheque_book.create_cheque_leaf",
-          args: {
-            company: frm.doc.company,
-            cheque_book: frm.doc.cheque_book,
-            bank_account: frm.doc.bank_account,
-            account_number: frm.doc.account_number,
-            bank_name: frm.doc.bank_name,
-            branch: frm.doc.branch,
-            issue_date: frm.doc.issue_date,
-            first_cheque_no: frm.doc.first_cheque_no,
-            last_cheque_no: frm.doc.last_cheque_no,
-          },
-          callback: function (r) {
-            frappe.msgprint(
-              __(
-                "Total " +
-                  frm.doc.number_of_leafs +
-                  " Cheque Leafs have been created!"
-              )
-            );
-          },
-        });
+        if(frm.doc.branch==undefined || frm.doc.branch==""){
+            frappe.msgprint("Branch is required in `Bank Account` to proceed.")
+        }else{
+          frappe.call({
+            method:
+              "akf_accounts.cheque_management.doctype.cheque_book.cheque_book.create_cheque_leaf",
+            args: {
+              company: frm.doc.company,
+              cheque_book: frm.doc.cheque_book,
+              bank_account: frm.doc.bank_account,
+              account_number: frm.doc.account_number,
+              bank_name: frm.doc.bank_name,
+              branch: frm.doc.branch,
+              issue_date: frm.doc.issue_date,
+              first_cheque_no: frm.doc.first_cheque_no,
+              last_cheque_no: frm.doc.last_cheque_no,
+            },
+            callback: function (r) {
+              frappe.msgprint(
+                __(
+                  "Total " +
+                    frm.doc.number_of_leafs +
+                    " Cheque Leafs have been created!"
+                )
+              );
+            },
+          });
+        }
       },
       __("Create")
     );
