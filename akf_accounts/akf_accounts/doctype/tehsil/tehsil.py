ee8ac67e2f8f793dcf86130f742164509a6f03ce
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-05 13:01:21 +0000

    Tehsil name must contain Alphabets - mubarrim

--- akf_accounts/akf_accounts/doctype/tehsil/tehsil.py
+++ akf_accounts/akf_accounts/doctype/tehsil/tehsil.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2024, Nabeel Saleem and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+import re
 
 class Tehsil(Document):
-	pass
+	def validate(self):
+		if not re.match("^[A-Z]+$", self.tehsil_name.upper()):
+			frappe.throw("Tehsil Name must contain only alphabets")
