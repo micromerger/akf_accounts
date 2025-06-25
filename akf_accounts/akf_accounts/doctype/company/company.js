420b43d6280f60c05d5394af971a1eb9b646f2c1
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-06-25 13:38:00 +0000

    company changes

--- akf_accounts/akf_accounts/doctype/company/company.js
+++ akf_accounts/akf_accounts/doctype/company/company.js
@@ -281,3 +281,38 @@ var disbale_coa_fields = function(frm, bool=true) {
 	frm.set_df_property("chart_of_accounts", "read_only", bool);
 	frm.set_df_property("existing_company", "read_only", bool);
 }
+
+cur_frm.cscript.change_abbr = function() {
+	var dialog = new frappe.ui.Dialog({
+		title: "Replace Abbr",
+		fields: [
+			{"fieldtype": "Data", "label": "New Abbreviation", "fieldname": "new_abbr",
+				"reqd": 1 },
+			{"fieldtype": "Button", "label": "Update", "fieldname": "update"},
+		]
+	});
+
+	dialog.fields_dict.update.$input.click(function() {
+		var args = dialog.get_values();
+		if(!args) return;
+		frappe.show_alert(__("Update in progress. It might take a while."));
+		return frappe.call({
+			method: "akf_accounts.akf_accounts.doctype.company.company.enqueue_replace_abbr",
+			args: {
+				"company": cur_frm.doc.name,
+				"old": cur_frm.doc.abbr,
+				"new": args.new_abbr
+			},
+			callback: function(r) {
+				if(r.exc) {
+					frappe.msgprint(__("There were errors."));
+					return;
+				}
+				dialog.hide();
+				cur_frm.refresh();
+			},
+			btn: this
+		})
+	});
+	dialog.show();
+}
