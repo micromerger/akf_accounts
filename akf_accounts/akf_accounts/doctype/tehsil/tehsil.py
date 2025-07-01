e77e874a2c500a8aa8807c2ff480591b30587524
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-07-01 09:08:32 +0000

    Allow spaces in Tehsil Name - mubarrim

--- akf_accounts/akf_accounts/doctype/tehsil/tehsil.py
+++ akf_accounts/akf_accounts/doctype/tehsil/tehsil.py
@@ -7,5 +7,5 @@ import re
 
 class Tehsil(Document):
 	def validate(self):
-		if not re.match("^[A-Z]+$", self.tehsil_name.upper()):
+		if not re.match("^[A-Z ]+$", self.tehsil_name.upper()):
 			frappe.throw("Tehsil Name must contain only alphabets")
