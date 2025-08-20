be98fdc4aa25f067d6622b5b9da08c0771e98b5a
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-08-20 13:41:09 +0000

    donor and lead

--- akf_accounts/hooks.py
+++ akf_accounts/hooks.py
@@ -125,7 +125,7 @@ doctype_js = {
 
 override_doctype_class = {
 	"Payment Entry": "akf_accounts.customizations.overrides.payment_entry.XPaymentEntry",
-	"Stock Entry": "akf_accounts.customizations.extends.xstock_entry.XStockEntry",
+"Stock Entry": "akf_accounts.customizations.extends.xstock_entry.XStockEntry",
 	# "Purchase Receipt" : "akf_accounts.customizations.extends.xpurchase_receipt.XAssetInvenPurchase",
     # "Purchase Invoice" : "akf_accounts.customizations.extends.xpurchase_invoice.XPurchaseInvoice",
     # "Sales Invoice": "akf_accounts.customizations.extends.xsales_invoice.XSalesInvoice",
