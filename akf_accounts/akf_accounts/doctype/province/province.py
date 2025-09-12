c0028fafa4e9bf4d52271d10c00ee2bddff6f981
nabeelsocialuse <nabeel.social.use@gmail.com>
2025-09-12 15:43:38 +0000

    deleted docs.

--- akf_accounts/akf_accounts/doctype/province/province.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Nabeel Saleem and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class Province(Document):
-	pass
