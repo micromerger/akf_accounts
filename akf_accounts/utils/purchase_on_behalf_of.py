9743daf71bac51f10251af9df1d9b5b210277f94
“nabeelsaleem333” <nabeel.saleem333@gmail.com>
2025-09-24 07:16:57 +0000

    program_details error fixed.

--- akf_accounts/utils/purchase_on_behalf_of.py
+++ akf_accounts/utils/purchase_on_behalf_of.py
@@ -6,13 +6,13 @@ from erpnext.accounts.utils import get_company_default
 def validate_donor_balance(self):
 	if(self.is_new()): return
 	if(not get_company_default(self.company, "custom_enable_accounting_dimensions_dialog", ignore_validation=True)): 
-		self.set("program_details", [])
+		self.set("custom_program_details", [])
 		return
 
-	donor_balance = sum([d.actual_balance for d in self.program_details])
+	donor_balance = sum([d.actual_balance for d in self.custom_program_details])
 	item_amount = sum([d.amount for d in self.items])
 
-	if(not self.program_details):
+	if(not self.custom_program_details):
 		frappe.throw("Balance is required to proceed further.", title='Donor Balance')
 	if(item_amount> donor_balance):
 		frappe.throw(f"Item amount: <b>Rs.{fmt_money(item_amount)}</b> exceeding the available balance: <b>Rs.{fmt_money(donor_balance)}</b>.", title='Donor Balance')
@@ -36,7 +36,7 @@ def make_on_behalf_of_gl_entries(self):
 			'company': self.company,
 		})
 	amount = sum([d.amount for d in self.items])
-	for row in self.program_details:
+	for row in self.custom_program_details:
 		difference_amount = amount if(row.actual_balance>=amount) else (amount - row.actual_balance)
 		amount = amount - difference_amount
 		make_branchB_expense_gl_entry(self, args, row, difference_amount)
